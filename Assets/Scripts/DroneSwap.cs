9365f47cdf7ac91f33cec1de87d07c76dff570bc
Aporoid <Aporoid@hotmail.com>
2020-03-01 22:47:05 -0600

    Basic implementation
    this doesn't need to stay, but it's a start

---
+++ Assets/Scripts/DroneSwap.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneSwap : MonoBehaviour
+{
+	//These are the cameras
+	[SerializeField]
+	private GameObject playerCamera;
+	[SerializeField]
+	private GameObject droneCamera;
+
+	private bool isInPlayerEyes;
+	private bool isInDroneEyes;
+
+	private void Start()
+	{
+		isInPlayerEyes = true;
+		isInDroneEyes = false;
+	}
+
+	private void Update()
+	{
+		CheckTransition();
+	}
+
+	private void CheckTransition()
+	{
+		if (Input.GetButtonDown("DroneAccess") && isInPlayerEyes == true && isInDroneEyes == false)
+		{
+			isInPlayerEyes = false;
+			isInDroneEyes = true;
+			playerCamera.SetActive(false);
+			droneCamera.SetActive(true);
+		}
+		else if (Input.GetButtonDown("DroneAccess") && isInDroneEyes == true && isInPlayerEyes == false)
+		{
+			isInPlayerEyes = true;
+			isInDroneEyes = false;
+			playerCamera.SetActive(true);
+			droneCamera.SetActive(false);
+		}
+	}
+}
