f218d7a693a556f50b345e1a2ef755ab6db28a0f
Aporoid <Aporoid@hotmail.com>
2020-03-09 18:36:17 -0500

    Adjusting idea of Drone UI

---
+++ Assets/Scripts/DroneSwapController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DroneSwapController : MonoBehaviour
+{
+    [SerializeField]
+    private Image droneSwapPrompt;
+
+    [SerializeField]
+    private GameObject labTrigger;
+    [SerializeField]
+    private GameObject worldTrigger;
+
+    [SerializeField]
+    private bool isInLab;
+    [SerializeField]
+    private bool isInWorld;
+
+    private void Start()
+    {
+        droneSwapPrompt.enabled = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isInLab == true && isInWorld == false)
+        {
+            droneSwapPrompt.enabled = false;
+        }
+        else if (isInLab == false && isInWorld == true)
+        {
+            droneSwapPrompt.enabled = true;
+        }
+
+    }
+}
